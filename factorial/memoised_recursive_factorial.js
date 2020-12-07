3a2d5e238337a944f8a1e3e374732ed34ff9152d
bethanHutt <bethan.hutt@yrbrands.com>
2020-12-07 17:33:22 +0000

    added memoised factorial

---
+++ factorial/memoised_recursive_factorial.js
@@ -0,0 +1,24 @@
+/*
+
+A memoised version of the recursive factorial solution.
+Essentially compiling a lookup table as the fn is run to
+reduce recursive calls to the fn.
+
+*/
+
+var memo = {};
+
+var factorial = function(n) {
+    if (n === 0) {
+        return 1;
+    } else if (memo.hasOwnProperty(n)) {
+        return memo[n];
+    } else {
+        var result = factorial(n - 1) * n;
+        memo[n] = result;
+        return result;
+    }
+};
+
+factorial(2);
+factorial(5);
